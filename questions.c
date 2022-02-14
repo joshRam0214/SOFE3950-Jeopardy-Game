31c1338efbd027470ff74ba2425a506defffe0a7
NathanBoj <77807505+NathanBoj@users.noreply.github.com>
2022-02-14 10:55:52 -0500

    Add files via upload

--- questions.c
+++ questions.c
@@ -15,60 +15,72 @@ void initialize_game(void)
 {
     // initialize each question struct and assign it to the questions array
     for (int a = 0; a < 12; a++) {
-        question[a].answered = false;
+        questions[a].answered = false;
     }
 
     //$200 questions -------------------------------->
     questions[0].value = 200;
     strcpy(questions[0].question, "When life gives you lemons...");
     strcpy(questions[0].answer, "make lemonade");
+    strcpy(questions[0].category, categories[0]);
 
     questions[1].value = 200;
     strcpy(questions[1].question, "A \"Mandalorian\" character is named Grogu & is also called the Child, but viewers dubbed him \"Baby\" this");
     strcpy(questions[1].answer, "Yoda");
+    strcpy(questions[1].category, categories[1]);
 
     questions[2].value = 200;
     strcpy(questions[2].question, "Pu-239 is an isotope of this silvery metal");
     strcpy(questions[2].answer, "plutonium");
+    strcpy(questions[2].category, categories[3]);
 
     //$400 questions -------------------------------->
     questions[3].value = 400;
     strcpy(questions[3].question, "I came, I saw, ...");
     strcpy(questions[3].answer, "I conquered");
+    strcpy(questions[3].category, categories[0]);
 
     questions[4].value = 400;
     strcpy(questions[4].question, "The devil didn't go down to Georgia, he went up to L.A. to open a nightclub & solve murders on this Netflix show");
     strcpy(questions[4].answer, "Lucifer");
+    strcpy(questions[4].category, categories[1]);
 
     questions[5].value = 400;
     strcpy(questions[5].question, "Sir Humphry Davy used electrolysis to isolate it, symbol K");
     strcpy(questions[5].answer, "potassium");
+    strcpy(questions[5].category, categories[2]);
 
     //$600 questions -------------------------------->
     questions[6].value = 600;
     strcpy(questions[6].question, "A legal principle: \"Ignorance of the law is...\"");
     strcpy(questions[6].answer, "no excuse");
+    strcpy(questions[6].category, categories[0]);
 
     questions[7].value = 600;
     strcpy(questions[7].question, "Billy Butcher has some serious issues with superheroes on this Amazon show & confronts them quite directly");
     strcpy(questions[7].answer, "The Boys");
+    strcpy(questions[7].category, categories[1]);
 
     questions[8].value = 600;
     strcpy(questions[8].question, "Atomic No. 20, it's needed for kids' teeth to grow");
     strcpy(questions[8].answer, "calcium");
+    strcpy(questions[8].category, categories[2]);
 
     //$800 questions -------------------------------->
     questions[9].value = 800;
     strcpy(questions[9].question, "Among AFI's 100 Greatest Movie Quotes of All Time:");
     strcpy(questions[9].answer, "phone home");
+    strcpy(questions[9].category, categories[0]);
 
     questions[10].value = 800;
     strcpy(questions[10].question, "An HBO Max series with \"That Damn\" this \"Weekend Update\" co-anchor examines issues like racial profiling & unemployment");
     strcpy(questions[10].answer, "Jaskaran");
+    strcpy(questions[10].category, categories[1]);
 
     questions[11].value = 800;
     strcpy(questions[11].question, "This element makes up about 25% of the mass in your average star");
     strcpy(questions[11].answer, "helium");
+    strcpy(questions[11].category, categories[2]);
 
 }
 
